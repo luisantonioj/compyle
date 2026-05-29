b790fce19c69519ccfd32ff82cb87cc8071a79dc
luisantonioj <luisantoniojopia@gmail.com>
2026-05-30 00:02:09 +0800

    feat: fix allowable screen layout for screen size

--- src/hooks/useIsWeb.ts
+++ src/hooks/useIsWeb.ts
@@ -2,10 +2,10 @@ import { useEffect, useState } from 'react';
 
 export function useIsWeb(): boolean {
   const [isWeb, setIsWeb] = useState(
-    () => typeof window !== 'undefined' && window.matchMedia('(min-width: 1024px)').matches
+    () => typeof window !== 'undefined' && window.matchMedia('(min-width: 768px)').matches
   );
   useEffect(() => {
-    const mq = window.matchMedia('(min-width: 1024px)');
+    const mq = window.matchMedia('(min-width: 768px)');
     const handler = (e: MediaQueryListEvent) => setIsWeb(e.matches);
     mq.addEventListener('change', handler);
     return () => mq.removeEventListener('change', handler);
